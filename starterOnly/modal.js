4f7b0739be6e1b0dd67630d8a91e9af7bc67ab63
hamzaoui12 <hamzaouimerina@gmail.com>
2024-03-21 14:58:58 +0100

    Feat: Ajout de la fonctionnalité au bouton (x)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,26 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+// Ajout du sélecteur pour le bouton de fermeture
+const closeBtn = document.querySelector(".close"); 
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+// Ajout de l'événement pour le bouton de fermeture
+closeBtn.addEventListener("click", closeModal); 
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 
