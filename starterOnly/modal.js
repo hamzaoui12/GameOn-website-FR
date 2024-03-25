655dd1d56a461e43e91cfa4e9588bf8cfd8d5607
hamzaoui12 <hamzaouimerina@gmail.com>
2024-03-25 10:42:08 +0100

    Feat: Ajout de la fonctionnalité au bouton (x)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,56 +10,26 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-// Ajout du sélecteur pour le bouton de fermeture
-const closeBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
-const form = document.querySelector("form[name='reserve']");
+// Ajout du sélecteur pour le bouton de fermeture
+const CloseBtn = document.querySelectorAll(".close");
 
-// launch modal event
+
+// launch modal 
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-// close modal event
-// Ajout de l'événement pour le bouton de fermeture
-closeBtn.addEventListener("click", closeModal);
 
-form.addEventListener("submit", validateForm);
-
-// launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form
+// close modal 
+CloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
 function closeModal() {
   modalbg.style.display = "none";
 }
 
 
-function validateForm(event) {
-  event.preventDefault(); 
 
-
-  let isValid = true;
-
-  formData.forEach((field) => {
-    const input = field.querySelector("input, textarea, select");
-
-
-    if (input.hasAttribute("required") && input.value.trim() === "") {
-      isValid = false;
-
-      field.classList.add("error");
-    } else {
-
-      field.classList.remove("error");
-    }
-
-
-  });
-
-
-  if (isValid) {
-    form.submit();
-  }
-}
 
 
